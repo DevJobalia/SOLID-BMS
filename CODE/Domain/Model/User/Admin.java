0366cb8500b6d5582e19cc59faf7801410623c43
DevJobalia <webdev.jobalia@gmail.com>
2025-06-22 09:36:36 +0530

    ADD: DEFINE USER

--- CODE/Domain/Model/User/Admin.java
+++ CODE/Domain/Model/User/Admin.java
@@ -3,4 +3,6 @@ package CODE.Domain.Model.User;
 public class Admin extends AUser{
     private String password;
 
+    // TODO: FUNCTIONS
+
 }
--- CODE/Domain/Model/User/Customer.java
+++ CODE/Domain/Model/User/Customer.java
@@ -1,5 +1,15 @@
 package CODE.Domain.Model.User;
 
+import java.util.List;
+
+import CODE.Domain.Model.Booking;
+import CODE.Domain.Model.Payment.APaymentMode;
+import CODE.Domain.Model.Payment.PaymentResult;
+
 public class Customer extends AUser{
-    
+    private List<APaymentMode> savedPaymentModes;
+    private List<Booking> bookings; // all bookings - saved, cancelled, refunded
+    private List<PaymentResult>  successfullBookings;
+
+    // TODO: FUNCTIONS
 }
